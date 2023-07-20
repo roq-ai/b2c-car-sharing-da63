e6f4ed143155d766eb0cd1585f31a5857c3af0c8
roq-ai <>
2023-07-20 13:43:08 +0000

    Initial commit

---
+++ src/interfaces/booking/index.ts
@@ -0,0 +1,23 @@
+import { UserInterface } from 'interfaces/user';
+import { CarInterface } from 'interfaces/car';
+import { GetQueryInterface } from 'interfaces';
+
+export interface BookingInterface {
+  id?: string;
+  date: any;
+  time: any;
+  user_id?: string;
+  car_id?: string;
+  created_at?: any;
+  updated_at?: any;
+
+  user?: UserInterface;
+  car?: CarInterface;
+  _count?: {};
+}
+
+export interface BookingGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  user_id?: string;
+  car_id?: string;
+}
---
+++ src/validationSchema/bookings/index.ts
@@ -0,0 +1,8 @@
+import * as yup from 'yup';
+
+export const bookingValidationSchema = yup.object().shape({
+  date: yup.date().required(),
+  time: yup.date().required(),
+  user_id: yup.string().nullable(),
+  car_id: yup.string().nullable(),
+});
